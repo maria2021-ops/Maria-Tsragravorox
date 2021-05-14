c3d3c86696b5afcca1772b7777fbc9cbe4181ece
maria2021-ops <maria_yeritsyan@edu.aua.am>
2021-05-14 20:49:02 +0400

    Create

--- server.js
+++ server.js
@@ -1,12 +1,95 @@
-const express = require("express");
-const app=express();
+require('dotenv').config()
 
-const PORT = 5001;
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser')
 
-app.get("/", (req, res) => {
-    res.send("Hello Armenia");
+// Google Auth
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '765373136675-22hjjjllhf63mvhq13oteq2o6fj7geig.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
+
+
+const PORT = 3100;
+
+// Middleware
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.render('index')
 })
 
-app.listen(PORT, () => {
-	console.log("Server Started on: " +PORT)
+app.get('/login', (req,res)=>{
+    res.render('login');
+})
+
+app.post('/login', (req,res)=>{
+    let token = req.body.token;
+
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+      }
+      verify()
+      .then(()=>{
+          res.cookie('session-token', token);
+          res.send('success')
+      })
+      .catch(console.error);
+
+})
+
+app.get('/dashboard', checkAuthenticated, (req, res)=>{
+    let user = req.user;
+    res.render('dashboard', {user});
+})
+
+app.get('/protectedRoute', checkAuthenticated, (req,res)=>{
+    res.send('This route is protected')
+})
+
+app.get('/logout', (req, res)=>{
+    res.clearCookie('session-token');
+    res.redirect('/login')
+
+})
+
+
+function checkAuthenticated(req, res, next){
+
+    let token = req.cookies['session-token'];
+
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+      verify()
+      .then(()=>{
+          req.user = user;
+          next();
+      })
+      .catch(err=>{
+          res.redirect('/login')
+      })
+
+}
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
 })
